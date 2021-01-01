90c257ab3b52fbdd6b0f18c1f8f8629f37647702
bobo <syswjb@126.com>
2021-01-01 11:38:14 +0800

    初始版本1.0。

---
+++ HelloProject/src/org/ccunix/model/Dog.java
@@ -0,0 +1,7 @@
+package org.ccunix.model;
+
+public class Dog {
+    public static void main(String[] args) {
+        System.out.println("我是一个Dog......");
+    }
+}
---
+++ HelloProject/src/org/ccunix/model/Person.java
@@ -0,0 +1,8 @@
+package org.ccunix.model;
+
+public class Person {
+    public static void main(String[] args) {
+        System.out.println("hello world!!!");
+    }
+
+}
